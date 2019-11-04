7a3edf3fe084a3445c6a2b053982c129a4d3b367
niezhaoxu123 <1445431738@qq.com>
2019-11-04 12:08:21 +0800

    nzx

---
+++ src/pages/nzx/nzx.js
@@ -0,0 +1,12 @@
+import React ,{Component} from 'react'
+
+class Nzx extends Component{
+    render(){
+        return(
+            <div>
+                聂兆旭
+            </div>
+        )
+    }
+}
+export default Nzx
