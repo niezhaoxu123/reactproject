5220c493b13a429f1ddff5c5e3fbc4d82d470701
bashuang <53343306+bashuang@users.noreply.github.com>
2019-11-04 11:55:51 +0800

    'ba-init'

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
 import React from 'react';
-
+import Test from './pages/test'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Test></Test>
     </div>
   );
 }
---
+++ src/pages/test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class Test extends React.Component{
+    render(){
+        return(
+            <div>888888888888</div>
+        )
+    }
+}
+export default Test
