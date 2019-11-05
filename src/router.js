e848823be7a5bc8002f1966201ef72a5d0593886
Tongqr <1011648352.qq.com>
2019-11-05 15:33:33 +0800

    l

--- src/router.js
+++ src/router.js
@@ -4,12 +4,11 @@ import loadable from './utils/loadable'
 //不用懒加载得方式
 // import Nzx from './pages/nzx/nzx'
 //使用懒加载得方式
-<<<<<<< HEAD
+
 
 // const Nzx=loadable(()=>import('./pages/nzx/nzx'))
 const Login=loadable(()=>import('./pages/login/login'))
-=======
->>>>>>> 2d5244b6db7dbcfdf92eca090b0de4015ef149e0
+
 const Admin=loadable(()=>import('./pages/admin/admin'))
 const Home=loadable(()=>import('./pages/home/home'))
 
@@ -18,19 +17,13 @@ class Router extends React.Component{
         return(
             <HashRouter>
                 <Switch>
-<<<<<<< HEAD
+
                     <Redirect exact from='/' to='/login'></Redirect>
                     <Route path='/login' component={Login}></Route>
                     <Route path='/admin' component={()=>{
                        return(
                            <Admin>
-=======
-                    <Redirect exact from='/' to='/admin'></Redirect>
-                    <Route path='/admin' component={()=>{
-                       return(
-                           <Admin>
                                {/*<Route path='/admin/home' component={Home}/>*/}
->>>>>>> 2d5244b6db7dbcfdf92eca090b0de4015ef149e0
                                <Route path='/admin/home' component={Home}></Route>
                            </Admin>
                        )
